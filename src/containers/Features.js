6ceecef0334cf1226ee7d8ebcf4e02e94281c6ee
parlop <parlop@gmail.com>
2017-10-04 21:50:38 -0500

    Update Features.js

--- src/containers/Features.js
+++ src/containers/Features.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from 'jquery';
 
 class Features extends React.Component {
 	constructor(props) {
@@ -6,8 +7,14 @@ class Features extends React.Component {
 	this.state={};
 	this.goRoot= this.goRoot.bind(this);
 	}
+	
+	componentDidMount(){
+        $(document).ready(function(){
+            $(".textfromjquery").text("This text is using Jquery!");
+        });
+    	}
 
-    goRoot(){
+    	goRoot(){
 		console.log('Button clicked, go to root');
 		this.props.history.push('/');
 	}
@@ -15,6 +22,7 @@ render() {
     return (      
         <div className="container-fluid">
             <div className="col-sm-12">
+	    	<p className="textfromjquery">loading...</p>
                 <p>Features page</p>
                 <button type="button" onClick={this.goRoot} className="btn btn-primary">Go to root</button>
             </div>
